549420413d52ebb3a8a615a0acf477b567a4b4fe
Erica Wright <ericaw21@gmail.com>
2017-03-17 10:56:04 -0700

    update doctor display styling

--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -1,7 +1,7 @@
 var Doctor = require('./../js/doctors.js').doctorModule;
 
 var displayDoctors = function(firstName, lastName, title, picture, specialties, education) {
-  $('#found-doctors').append('<li>' + firstName + ' ' + lastName + ', ' + title + '<br><img src="' + picture + '"><br>' + 'Specialties: ' + specialties + '<br>Education: ' + education + '</li><br>');
+  $('#found-doctors').append('<li class="doctor-entry"><img src="' + picture + '" class="doctor-picture"><span class="doctor-name">' + firstName + ' ' + lastName + ', ' + title + '</span><br>' + '<span class="doctor-bold">Specialties</span>: ' + specialties + '<br><span class="doctor-bold">Education</span>: ' + education + '</li><br>');
 };
 
 $(document).ready(function() {
