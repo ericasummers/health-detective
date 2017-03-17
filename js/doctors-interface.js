a9fd316ef95e082f3dfde1d0f7dae1730a01f73b
Erica Wright <ericaw21@gmail.com>
2017-03-17 12:06:07 -0700

    update styling to display doctor gender

--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -1,11 +1,18 @@
 var Doctor = require('./../js/doctors.js').doctorModule;
 
-var displayDoctors = function(firstName, lastName, title, picture, specialties, education) {
-  $('#found-doctors').append('<li class="doctor-entry"><img src="' + picture + '" class="doctor-picture"><span class="doctor-name">' + firstName + ' ' + lastName + ', ' + title + '</span><br>' + '<span class="doctor-bold">Specialties</span>: ' + specialties + '<br><span class="doctor-bold">Education</span>: ' + education + '</li><br>');
+var displayDoctors = function(firstName, lastName, title, picture, specialties, education, gender) {
+  if (gender === 'female') {
+    icon = '<i class="fa fa-female" aria-hidden="true"></i>';
+  } else if (gender === 'male'){
+    icon = '<i class="fa fa-male" aria-hidden="true"></i>';
+  }
+  $('#found-doctors').append('<li class="doctor-entry"><img src="' + picture + '" class="doctor-picture"><span class="doctor-name">' + firstName + ' ' + lastName + ', ' + title + '</span> ' + icon + '<br>' + '<span class="doctor-bold">Specialties</span>: ' + specialties + '<br><span class="doctor-bold">Education</span>: ' + education + '</li><br>');
+  console.log(gender);
 };
 
 $(document).ready(function() {
 var newDoctor = new Doctor();
+newDoctor.findSpecialties();
 
   $('#search-form').submit(function(event) {
     event.preventDefault();
