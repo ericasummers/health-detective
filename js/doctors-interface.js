5b649182f591a7bc4b532a4db3540c60e3d0e5a1
Erica Wright <ericaw21@gmail.com>
2017-03-17 10:16:33 -0700

    create callback function to display doctor results

--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -1,12 +1,17 @@
 var Doctor = require('./../js/doctors.js').doctorModule;
 
+var displayDoctors = function(firstName, lastName, title, picture, specialties, education) {
+  $('#found-doctors').append('<li>' + firstName + ' ' + lastName + ', ' + title + '<br><img src="' + picture + '"><br>' + 'Specialties: ' + specialties + '<br>Education: ' + education + '</li><br>');
+}
+
 $(document).ready(function() {
 var newDoctor = new Doctor();
 
   $('#search-form').submit(function(event) {
     event.preventDefault();
+    $('#found-doctors').empty();
     var condition = $('#condition').val();
-    newDoctor.search(condition);
+    newDoctor.search(condition, displayDoctors);
 
   });
 
--- js/doctors.js
+++ js/doctors.js
@@ -3,20 +3,29 @@ var apiKey = require('./../.env').ApiKey;
 function Doctor() {
 }
 
-Doctor.prototype.search = function(condition) {
+Doctor.prototype.search = function(condition, displayDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + condition + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=25&user_key=' + apiKey).then(function(response) {
-    for (var i = 0; i <= response.data.length; i++) {
+    for (var i = 0; i < response.data.length; i++) {
       var doctorFirstName = response.data[i].profile.first_name;
       var doctorLastName = response.data[i].profile.last_name;
-      var doctorPicture = response.data[i].profile.image_url;
       var doctorTitle = response.data[i].profile.title;
+      var doctorPicture = response.data[i].profile.image_url;
       var doctorSpecialties = [];
       for (var j = 0; j < response.data[i].specialties.length; j++) {
         doctorSpecialties.push(response.data[i].specialties[j].name);
       }
-      $('#found-doctors').append('<li>' + doctorFirstName + ' ' + doctorLastName + ', ' + doctorTitle + '<br><img src="' + doctorPicture + '"><br>' + 'Specialties: ' + doctorSpecialties + '</li><br>');
+      var doctorEducation = [];
+      for (var k = 0; k < response.data[i].educations.length; k++) {
+        var schoolName = response.data[i].educations[k].school;
+        var schoolDegree = response.data[i].educations[k].degree;
+        var gradYear = response.data[i].educations[k].graduation_year;
+        doctorEducation.push(schoolName + ' (' + schoolDegree + ') ' + '- ' + gradYear);
+
+      }
+      displayDoctors(doctorFirstName, doctorLastName, doctorTitle, doctorPicture, doctorSpecialties, doctorEducation);
     }
     console.log(response);
+
   }).fail(function(error) {
 
   });
